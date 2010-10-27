0bd60562fe2eb31a6f2cbb3a54c714b8ba657df3
Pablo Mouzo <pablomouzo@gmail.com>
2010-10-27 12:30:05 -0300

    hack for autocomplete bug in css mode

--- custom/autocomplete.el
+++ custom/autocomplete.el
@@ -2,3 +2,7 @@
 ;(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/autocomplete-dict")
 (require 'auto-complete-config)
 (ac-config-default)
+
+;; CSS autocomplete inifinite loop hacks
+(add-to-list 'ac-css-value-classes
+	     '(border-width "thin" "medium" "thick" "inherit"))
