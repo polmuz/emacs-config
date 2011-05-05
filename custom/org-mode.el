1bba525ece4381c26ff7bb9ce02c87d6bf626061
Pablo Mouzo <pablom@olx.com>
2011-05-05 16:17:53 -0300

    Cleaned up org-mode config, Im starting to use it again

--- custom/org-mode.el
+++ custom/org-mode.el
@@ -7,33 +7,9 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
-(setq org-mobile-inbox-for-pull "~/orgfiles/orgmobile-pull-inbox.org")
 
-(setq org-directory "~/orgfiles")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
+(setq org-agenda-files (list "~/orgfiles/work.org"
+                             "~/orgfiles/home.org"
+			     "~/orgfiles/projects.org"))
 
-(define-key global-map "\C-cc" 'org-capture)
-
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline
-			   "~/orgfiles/gtd.org"
-			   "Tasks")
-	 "* TODO %?\n  %i\n  %a")
-        ("j" "Work Journal" entry (file+datetree
-			      "~/orgfiles/work-journal.org")
-	 "* %?\n %U\n  %i\n\n")
-	("a" "Accomplishments" entry (file
-			      "~/orgfiles/accomplishments.org")
-	 "* %?\n %U\n  %i\n\n")
-	("c" "Coding notes" entry (file
-			      "~/orgfiles/coding-notes.org")
-	 "* %?\n %U\n  %i\n %a\n")
-	)
-      )
-
-(setq org-mobile-directory "~/orgfiles/orgmobile")
-
-(setq org-mobile-files '("~/orgfiles/work-journal.org"
-			 "~/orgfiles/accomplishments.org"))
-
-
+(setq org-log-done 'time)
