cd3b2e957cbec1b5b76736e5b0297d6fa53d2a50
Pablo Mouzo <pablomouzo@gmail.com>
2012-11-13 17:18:41 -0300

    Flymake for python with pylint

--- init.el
+++ init.el
@@ -71,6 +71,23 @@
 
 (global-auto-revert-mode t)
 
+;; Python config
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "epylint" (list local-file))))
+
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
+;; Auto-start flymake-mode when you go into python-mode
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (flymake-mode)))
 
 ;; JS config
 (add-hook 'js-mode-hook
